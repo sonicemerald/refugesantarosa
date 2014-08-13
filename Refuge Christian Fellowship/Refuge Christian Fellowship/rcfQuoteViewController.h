089132167215741e9b38371cf7f5b1067942d598
micah gemmell <sonicemerald@gmail.com>
2014-08-13 12:03:44 -0700

    added splash image, opening quote page (static for now.)

---
+++ Refuge Christian Fellowship/Refuge Christian Fellowship/rcfQuoteViewController.h
@@ -0,0 +1,15 @@
+//
+//  rcfMoreViewController.h
+//  Refuge Christian Fellowship
+//
+//  Created by Micah Gemmell on 8/13/14.
+//  Copyright (c) 2014 RefugeCF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rcfQuoteViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *QuoteBody;
+@property (weak, nonatomic) IBOutlet UILabel *dothQuoted;
+
+@end
---
+++ Refuge Christian Fellowship/Refuge Christian Fellowship/rcfQuoteViewController.m
@@ -0,0 +1,22 @@
+//
+//  rcfMoreViewController.m
+//  Refuge Christian Fellowship
+//
+//  Created by Micah Gemmell on 8/13/14.
+//  Copyright (c) 2014 RefugeCF. All rights reserved.
+//
+
+#import "rcfQuoteViewController.h"
+
+@implementation rcfQuoteViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
