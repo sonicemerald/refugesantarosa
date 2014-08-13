8c20ddce4c7cecac58cf270478416ece912881f5
micah gemmell <sonicemerald@gmail.com>
2014-08-13 12:27:22 -0700

    cleaning up a little

--- Refuge Christian Fellowship/Refuge Christian Fellowship/rcfQuoteViewController.m
+++ Refuge Christian Fellowship/Refuge Christian Fellowship/rcfQuoteViewController.m
@@ -1,14 +1,19 @@
 //
-//  rcfMoreViewController.m
+//  rcfQuoteViewController.m
 //  Refuge Christian Fellowship
 //
 //  Created by Micah Gemmell on 8/13/14.
 //  Copyright (c) 2014 RefugeCF. All rights reserved.
 //
+// The idea for this view controller is to randomly display a small quote everytime
+// a user opens the app. The ideal situation is one where the quote pool can be updated
+// externally.
 
 #import "rcfQuoteViewController.h"
 
 @implementation rcfQuoteViewController
+
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
